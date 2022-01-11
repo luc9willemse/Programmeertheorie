80fb203b1deb827cec18401d89e5567bf3482f91
Luc Willemse <lucwillemse@live.nl>
2022-01-11 21:28:02 +0100

    implemented random function

--- Code/lijnvoering.py
+++ Code/lijnvoering.py
@@ -7,11 +7,37 @@ Programmeertheorie
 lijnvoering.py:
 this file contains the algoritems that find the lijnvoering
 """
-
 import data
+import network
+import random
+import get_data
 
-def random_traject_generater():
+def random_traject_generater(data):
     """
+    data    :   datasets (dict of trajects)
+
     this function picks random trajects
     """
-    pass
+    number_of_trajects = random.randint(1, 20)
+    list_of_traject = []
+    total_time = 0
+    for i in range(number_of_trajects):
+        traject = list(data.keys())[random.randint(0,(len(data)-1))]
+        list_of_traject.append(traject)
+        total_time += int(data[traject])
+
+    teller = 0
+    list_of_stations = get_data.list_of_stations(data)
+    for traject in list_of_traject:
+        for station in traject:
+            if station in list_of_stations:
+                teller += 1
+                list_of_stations.remove(station)
+
+    fraction = teller / len(get_data.list_of_stations(data))
+
+    return [list_of_traject, total_time, number_of_trajects, fraction]
+
+
+if __name__ == "__main__":
+    print(random_traject_generater(network.find_all_trajects(data.generate_dict()["ConnectiesHolland.csv"], 120)))
--- Code/network.py
+++ Code/network.py
@@ -1,9 +1,28 @@
+"""
+Name            :   Luc Willemse, Minne Sandstra, Stijn Maatje
+UvAnetID        :   14012391, 12261440, 12276529
+Project group   :   10
+Programmeertheorie
+
+network.py:
+this file makes a networkx graph of the stations (nodes) en the connections
+between the stations (edges). this file also contains a method that gives all
+the possible trajects
+"""
+
 import networkx as nx
 import matplotlib.pyplot as plt
 import data
 import get_data
 
 def generate_graph(data):
+    """
+    data    :   dataset
+
+    generates a graph of the data
+
+    return  :   graph
+    """
     G = nx.Graph()
 
     for connectie, time in data.items():
@@ -12,6 +31,15 @@ def generate_graph(data):
     return G
 
 def find_all_trajects(data, max_time):
+    """
+    data        :   dataset
+    max_time    :   max time that a traject can take
+
+    creates a dict with all the possible trajects as keys and the traject time
+    as value
+
+    return  :   dict
+    """
     dict_trajects = {}
     G = generate_graph(data)
     for departure_station in get_data.list_of_stations(data):
@@ -28,7 +56,7 @@ def find_all_trajects(data, max_time):
                             time += int(data[(trajects[teller+1], i)])
                             teller += 1
 
-                    if time < (max_time + 1):
+                    if time < (max_time + 1) and tuple(trajects)[::-1] not in dict_trajects:
                         dict_trajects[tuple(trajects)] = time
 
     return dict_trajects
